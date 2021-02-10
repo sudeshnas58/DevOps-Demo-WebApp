b142c83769effb3a2bad290cc3e11355a224fcc9
sudeshnas58 <77554663+sudeshnas58@users.noreply.github.com>
2021-02-10 15:40:46 -0500

    Update acat.java
    Updated IP

--- Acceptancetest/src/test/java/acceptancetest/acat.java
+++ Acceptancetest/src/test/java/acceptancetest/acat.java
@@ -12,8 +12,10 @@ public class acat {
 	public HtmlUnitDriver driver;
 	//public WebDriver driver;
 	public WebDriverWait wait;
-	public String URL = "http://52.20.56.83:8080/ProdWebapp";
-	public String URL1 = "http://52.20.56.83:8080/ProdWebapp/avncreatepage.jsp";
+	//public String URL = "http://52.20.56.83:8080/ProdWebapp";
+	//public String URL1 = "http://52.20.56.83:8080/ProdWebapp/avncreatepage.jsp";	
+	public String URL = "http://54.236.54.150:8080/ProdWebapp";
+	public String URL1 = "http://54.236.54.150:8080/ProdWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	
