0aa1e28170ee981c46c55540329b179242f184f5
sudeshnas58 <77554663+sudeshnas58@users.noreply.github.com>
2021-02-12 14:55:30 -0500

    Update ftat.java
    Updated ip

--- functionaltest/src/test/java/functionaltest/ftat.java
+++ functionaltest/src/test/java/functionaltest/ftat.java
@@ -20,8 +20,8 @@ public class ftat {
 	//public String URL1 = "http://52.20.56.83:8080/QAWebapp/avncreatepage.jsp";
 	
 	
-	public String URL = "http://34.224.4.235:8080/QAWebapp/";
-	public String URL1 = "http://34.224.4.235:8080/QAWebapp/avncreatepage.jsp";
+	public String URL = "http://34.229.68.17:8080/QAWebapp/";
+	public String URL1 = "http://34.229.68.17:8080/QAWebapp/avncreatepage.jsp";
 	//public String URL = "http://localhost:8080/AVNCommunication-1.0/avnlogin.jsp";
 	//public String URL1 = "http://localhost:8080/AVNCommunication-1.0/avncreatepage.jsp";
 	 
